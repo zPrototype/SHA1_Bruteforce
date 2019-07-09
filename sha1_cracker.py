1df1c32365aa378b461cd457f30801bb5799e217
zPrototype <43825360+zPrototype@users.noreply.github.com>
2019-07-09 13:19:21 +0200

    Add files via upload

---
+++ sha1_cracker.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+import hashlib
+import time
+
+ts = time.time()
+
+if len(sys.argv) != 3:
+    print("Usage:")
+    print("./sha1_cracker.py [HASHFILE] [WORDLIST] \n")
+    sys.exit
+
+hashes = set()
+with open(sys.argv[1], "r") as hashfile:
+    for line in hashfile:
+        hashes.add(line.strip())
+
+print("Start cracking...")
+with open(sys.argv[2], "r") as wordlist:
+    for line in wordlist:
+        line = line.strip('\n')
+        sha1_hash = hashlib.sha1(line.encode()).hexdigest()
+        if sha1_hash in hashes:
+            print("Found matching hash: " + sha1_hash)
+            print("Password is: " + line)
+
+td = time.time() - ts
+print("Done in " + str(td) + " sec.")
